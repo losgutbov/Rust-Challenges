bea12050c636ca3e32cad32789485311123b3c63
Augusto Lima <losgutbov@gmail.com>
2025-01-10 18:18:37 -0300

    Understanding more the difference between statement and expression

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,10 @@
-fn five() -> i32{
-    5
+fn plus_one(x: i32) -> i32{
+    let one: i32 = 1;
+    println!("Ora ora!!!");
+    x + one
 }
 fn main() {
-    let x = five();
+    let x = plus_one(5);
 
     println!("The value of x is: {x}")
 }
