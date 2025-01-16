b36692b0f6f0dde97402fa0c078cc9814b708320
Augusto Lima <losgutbov@gmail.com>
2025-01-16 09:15:05 -0300

    assigning the result of an if expression to a variable

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -10,4 +10,8 @@ fn main() {
     } else {
         println!("number is not divisible by 4, 3 or 2");
     }
+
+    let condition = true;
+    let number_2 = if condition {5} else {6};
+    println!("The value of number is: {number_2}")
 }
