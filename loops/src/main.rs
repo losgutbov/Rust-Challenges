706f11aa586ceb3adda977ff42e60d7cda34a446
Augusto Lima <losgutbov@gmail.com>
2025-01-18 16:15:21 -0300

    Using a for loop in a array

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,47 +1,55 @@
-fn loop_label() {
-    let mut count = 0;
+// fn loop_label() {
+//     let mut count = 0;
 
-    'counting_up: loop{
-        println!("count = {count}");
+//     'counting_up: loop{
+//         println!("count = {count}");
 
-        let mut remaining = 10;
+//         let mut remaining = 10;
 
-        loop{
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
+//         loop{
+//             println!("remaining = {remaining}");
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
 
-        count += 1;
-    }
+//         count += 1;
+//     }
 
-    println!("End count = {count}");
-}
+//     println!("End count = {count}");
+// }
 
-fn while_loop(){
-    let mut number = 3;
+// fn while_loop(){
+//     let mut number = 3;
 
-    while number !=0 {
-        println!("{number}!");
+//     while number !=0 {
+//         println!("{number}!");
 
-        number -= 1;
-    }
+//         number -= 1;
+//     }
 
-    println!("LIFTOFF!!!");
-}
+//     println!("LIFTOFF!!!");
+// }
+
+// fn iteration_while(){
+//     let a = [10, 20, 30, 40, 50];
+//     let mut index = 0;
+
+//     while index < 5 {
+//         println!("the value is {}", a[index]);
+
+//         index +=1;
+//     }
+// }
 
 fn main(){
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-        println!("the value is {}", a[index]);
-
-        index +=1;
+    for element in a {
+        println!("the value id: {element}");
     }
 }
