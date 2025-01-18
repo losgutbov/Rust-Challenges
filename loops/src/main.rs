ee25a7c191547a283f1807ed64fd2ffab537ec42
Augusto Lima <losgutbov@gmail.com>
2025-01-18 16:20:33 -0300

    Using a range in for loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -46,10 +46,17 @@
 //     }
 // }
 
-fn main(){
-    let a = [10, 20, 30, 40, 50];
+// fn for_array_loop(){
+//     let a = [10, 20, 30, 40, 50];
 
-    for element in a {
-        println!("the value id: {element}");
+//     for element in a {
+//         println!("the value id: {element}");
+//     }
+// }
+
+fn main(){
+    for number in (1..4).rev() {
+        println!("{number}");
     }
+    println!("LIFTOFF!!!");
 }
