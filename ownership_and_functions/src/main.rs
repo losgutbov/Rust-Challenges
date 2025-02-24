d54f7d59216b6b5210a1f28d92f6b2f7da6cfa76
Augusto Lima <losgutbov@gmail.com>
2025-02-24 18:21:36 -0300

    More about ownership

--- ownership_and_functions/src/main.rs
+++ ownership_and_functions/src/main.rs
@@ -1,17 +1,45 @@
 fn main() {
-    let s = String::from("hello");
+    // let s = String::from("hello");
 
-    takes_ownership(s);
+    // takes_ownership(s);
 
-    let x = 5;
+    // let x = 5;
 
-    makes_copy(x);
+    // makes_copy(x);
+
+    // let _s1 = gives_ownership();
+
+    // let s2 = String::from("hello");
+
+    // let _s3 = takes_and_gives_back(s2);
+
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{s2}' is {len}.");
 }
 
-fn takes_ownership(some_string: String){
-    println!("{some_string}");
+fn calculate_length(s:String) -> (String, usize){
+    let length = s.len();
+
+    (s,length)
 }
 
-fn makes_copy(some_integer: i32){
-    println!("{some_integer}");
-}
+// fn gives_ownership() -> String{
+//     let some_string = String::from("yours");
+
+//     some_string
+// }
+
+// fn takes_and_gives_back(a_string: String) -> String{
+//     a_string
+// }
+
+// fn takes_ownership(some_string: String){
+//     println!("{some_string}");
+// }
+
+// fn makes_copy(some_integer: i32){
+//     println!("{some_integer}");
+// }
